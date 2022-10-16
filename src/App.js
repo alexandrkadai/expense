6755a84432f78335169c852c5d0c9e99fc2ad5cc
alex kadai <alexandrkadai@gmail.com>
2022-10-16 23:40:10 +0300

    Expense container, item and date

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import ExpenseItem from './components/exspense/ExpenseItem.component';
+import ExpnseContainer from './components/expense-container/expense-container.component';
 function App() {
   const expenses = [
     {
@@ -24,22 +24,7 @@ function App() {
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}></ExpenseItem>
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}></ExpenseItem>{' '}
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}></ExpenseItem>{' '}
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}></ExpenseItem>
+      <ExpnseContainer data={expenses} />
     </div>
   );
 }
